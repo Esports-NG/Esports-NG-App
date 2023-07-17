cc1cf32ab68465e94f199d80122c45d00ebbc8ca
Michael Timi <michtimiael@gmail.com>
2023-07-17 22:24:54 +0100

    update

--- lib/ui/auth/register.dart
+++ lib/ui/auth/register.dart
@@ -1,13 +1,18 @@
+import 'dart:io';
+
 import 'package:e_sport/data/repository/auth_repository.dart';
 import 'package:e_sport/ui/widget/custom_text.dart';
 import 'package:e_sport/ui/widget/custom_textfield.dart';
 import 'package:e_sport/ui/widget/custom_widgets.dart';
 import 'package:e_sport/util/colors.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:nigerian_states_and_lga/nigerian_states_and_lga.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -19,8 +24,17 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   static final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final authController = Get.put(AuthRepository());
+  String stateValue = NigerianStatesAndLGA.allStates[0];
+  String lgaValue = 'Select a Local Government Area';
+  String selectedLGAFromAllLGAs = NigerianStatesAndLGA.getAllNigerianLGAs()[0];
+  List<String> statesLga = [];
+
   bool isHiddenPassword = true;
   bool? isChecked = false;
+  bool boolPass = false;
+  int pageCount = 0;
+  var number = "";
+  var bizNo = "";
 
   void _togglePasswordView() {
     setState(() {
@@ -85,7 +99,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
               ),
               Gap(Get.height * 0.02),
               CustomText(
-                title: 'Email address',
+                title: 'Fullname',
                 color: AppColor().primaryWhite,
                 textAlign: TextAlign.center,
                 fontFamily: 'GilroyRegular',
@@ -93,20 +107,18 @@ class _RegisterScreenState extends State<RegisterScreen> {
               ),
               Gap(Get.height * 0.01),
               CustomTextField(
-                hint: "Email address",
-                textEditingController: authController.emailController,
+                hint: "Johnny Doe",
+                textEditingController: authController.fNameController,
                 validate: (value) {
                   if (value!.isEmpty) {
-                    return 'Email address must not be empty';
-                  } else if (!RegExp(
-                          r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
-                      .hasMatch(value)) {
-                    return "enter a valid email address";
+                    return 'Fullname must not be empty';
+                  } else if (!RegExp(r'[A-Za-z]+$').hasMatch(value)) {
+                    return "Please enter only letters";
                   }
                   return null;
                 },
               ),
-              Gap(Get.height * 0.05),
+              Gap(Get.height * 0.02),
               CustomText(
                 title: 'Password',
                 color: AppColor().primaryWhite,
@@ -115,38 +127,65 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 size: Get.height * 0.018,
               ),
               Gap(Get.height * 0.01),
-              CustomTextField(
-                hint: "Password",
-                textEditingController: authController.passwordController,
-                obscure: isHiddenPassword,
-                validate: (value) {
-                  if (value!.isEmpty) {
-                    return 'password must not be empty';
-                  } else if (value.length < 4) {
-                    return 'password must not be less than 8 character';
-                  }
-                  return null;
-                },
-                suffixIcon: InkWell(
-                  onTap: _togglePasswordView,
-                  child: Icon(
-                    isHiddenPassword ? Icons.visibility : Icons.visibility_off,
-                    color: isHiddenPassword
-                        ? Colors.grey.withOpacity(0.5)
-                        : AppColor().pureBlackColor,
+              IntlPhoneField(
+                  disableLengthCheck: true,
+                  keyboardType: Platform.isIOS
+                      ? const TextInputType.numberWithOptions(
+                          signed: true, decimal: false)
+                      : TextInputType.phone,
+                  flagsButtonMargin: const EdgeInsets.only(left: 8),
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 13,
+                      fontStyle: FontStyle.normal,
+                      fontFamily: 'GilroyMedium',
+                      fontWeight: FontWeight.w600,
+                      height: 1.7),
+                  decoration: InputDecoration(
+                    fillColor: AppColor().lightBlack,
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 15),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: AppColor().textFieldColor,
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(10)),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: AppColor().textFieldColor, width: 1),
+                        borderRadius: BorderRadius.circular(10)),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: AppColor().textFieldColor, width: 1),
+                        borderRadius: BorderRadius.circular(10)),
+                    hintText: 'Phone Number',
+                    hintStyle: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 13,
+                        fontStyle: FontStyle.normal,
+                        fontFamily: 'GilroyMedium',
+                        fontWeight: FontWeight.w600,
+                        height: 1.7),
                   ),
-                ),
-              ),
-              Gap(Get.height * 0.02),
-              CustomText(
-                title: 'Forgot password?',
-                color: AppColor().primaryGreen,
-                textAlign: TextAlign.center,
-                fontFamily: 'GilroyRegular',
-                weight: FontWeight.w600,
-                size: Get.height * 0.02,
-                underline: TextDecoration.underline,
-              ),
+                  controller: authController.phoneNoController,
+                  initialCountryCode: "NG",
+                  validator: (data) {
+                    number = data!.number;
+                    if (number.length < 10 || number.length > 10) {
+                      return "Invalid phone number";
+                    } else if (!RegExp(r'^[0-9]+$').hasMatch(number)) {
+                      return "Please enter only digits";
+                    }
+                    return null;
+                  },
+                  onChanged: (data) {
+                    number = data.countryCode;
+                    if (kDebugMode) {
+                      print("number is ${data.countryCode}");
+                    }
+                  }),
               Gap(Get.height * 0.15),
               CustomFillButton(
                 buttonText: 'Log in',
