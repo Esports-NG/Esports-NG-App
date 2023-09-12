8eecf4f6faeece001bbca6772d27eddcc20bc288
Michael Timi <michtimiael@gmail.com>
2023-09-12 20:47:12 +0100

    app binding update

--- lib/di/app_binding.dart
+++ lib/di/app_binding.dart
@@ -1,8 +1,9 @@
+import 'package:e_sport/data/repository/auth_repository.dart';
 import 'package:get/get.dart';
 
 class AppBinding extends Bindings {
   @override
   void dependencies() {
-    // Get.put(AuthRepository(), permanent: true);
+    Get.put(AuthRepository(), permanent: true);
   }
 }
