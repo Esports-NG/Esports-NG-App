d22006cf31860b804341ace2b697bd12deff12ed
Esports NG <admin@esportsng.com>
2025-02-10 06:20:31 +0100

    dfgfds

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ void main() async {
   final notificationService = NotificationService();
   await notificationService.initialize();
   runApp(const ESportApp());
-  configLoading();
+  configLoading(); 
 }
 
 void configLoading() {
